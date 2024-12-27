2a7f1d3889eb6559c516ce2ae0587f1fda305515
Muhammed Mishab <mishabvibes@gmail.com>
2024-12-27 09:17:03 +0530

    Update dev.js for console this code

--- dev.js
+++ dev.js
@@ -19,3 +19,9 @@ function sumArray(arr) {
 const total = sumArray(numbers);
 console.log(`Sum: ${total}`);           // Output: Sum: 9
 console.log(`Average: ${total/3}`);     // Output: Average: 3
+
+console.log(count);                     // Output: 0
+console.log(numbers);                   // Output: []
+increment();
+console.log(numbers.push(count));       // Output: 1
+console.log(`Final state: ${count}`);   // Output: Final state: 1
